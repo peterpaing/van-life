425c053209e3fc56fa0656f24d592542769fb07c
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-07-26 16:34:07 +0630

    Finsihed Vans page section

--- src/Pages/Vans.jsx
+++ src/Pages/Vans.jsx
@@ -18,17 +18,33 @@ export default function Vans(){
 
     useEffect(()=>{vans()},[])
 
+    
+
     const renderVans = vanData.map(van=>{
+
+        const typeColor = van.type === 'simple' 
+        ? { backgroundColor: '#E17654' } 
+        : van.type === 'luxury' 
+        ? { backgroundColor: '#161616' } 
+        : { backgroundColor: '#115E59' }
+
         return (
             <div className='van-container' key={van.id}>
-                <img src={van.imageUrl} alt='${van.name} image'/>
+                <img src={van.imageUrl} alt={`${van.name} image`}/>
                 <p>{van.name}</p>
                 <span>${van.price}/day</span>
-                <p>{van.type}</p>
+                <p className='type' style={typeColor}>{van.type}</p>
             </div>
         )
     })
 
-    return renderVans
+    return (
+        <section className='vans-data'>
+        <h2>Explore our van options</h2>
+        <div className='vans'>
+        {renderVans}
+        </div>
+        </section>
+    )
 }
 
