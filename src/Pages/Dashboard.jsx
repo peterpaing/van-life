0254b308ce1b2e5a2091215995e319e93dbdbf02
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-07-30 23:30:08 +0630

    Render saved vans in local strorage to host/dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,33 @@
-import { IoStar } from "react-icons/io5";
+import { useState } from "react"
+import { IoStar } from "react-icons/io5"
 import {Link} from 'react-router'
 
 export default function Dashboard(){
+   const [savedVans, setSavedVans] = useState(() => {
+    const vans = []
+
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+
+        if (key && key.startsWith("van")) {
+            const van = JSON.parse(localStorage.getItem(key))
+            vans.push(van)
+        }
+    }
+
+    return vans
+})|| []
+
+const van = savedVans.map(van => (
+    <div className="saved-van" key={van.id}>
+        <img src={van.imageUrl} alt={`${van.name} image`} />
+        <div>
+            <p>{van.name}</p>
+            <span>${van.price}/day</span>
+        </div>
+    </div>
+))
+
     return (
         <section className="dashboard-container">
             <div className="income">
@@ -16,8 +42,11 @@ export default function Dashboard(){
             </div>
 
             <div className="listed-vans">
+                <div className="listed-van-headline">
                 <h3>Your listed vans</h3>
                 <Link to="/vans">View all</Link>
+                </div>
+                {savedVans ? van :<h4>There is no saved vans</h4>}
             </div>
         </section>
     )
