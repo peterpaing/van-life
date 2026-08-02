af68366b5b5a0f8e733ebb422e7ed2a60048fbf3
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-08-02 23:03:28 +0630

    Added filter feature to Vans page

--- src/Pages/Vans.jsx
+++ src/Pages/Vans.jsx
@@ -7,6 +7,7 @@ export default function Vans(){
 
     const [vanData , setVanData]= useState([])
     const[isLoading , setIsLoading] = useState(true)
+    const [typeFilter, setTypeFilter] = useState(null)
 
     const vans = async()=>{
         setIsLoading(true)
@@ -24,9 +25,21 @@ export default function Vans(){
 
     useEffect(()=>{vans()},[])
 
-    
+     const filteredVans = typeFilter
+        ? vanData.filter(van => van.type === typeFilter)
+        : vanData
 
-    const renderVans = vanData.map(van=>{
+        const getButtonStyle = (type) => {
+            if (type !== typeFilter) return {}
+
+            return type === "simple"
+                ? { backgroundColor: "#E17654", color: "white" }
+                : type === "luxury"
+                ? { backgroundColor: "#161616", color: "white" }
+                : { backgroundColor: "#115E59", color: "white" }
+        }
+
+    const renderVans = filteredVans.map(van=>{
 
         const typeColor = van.type === 'simple' 
         ? { backgroundColor: '#E17654' } 
@@ -49,6 +62,12 @@ export default function Vans(){
     return (
         <section className='vans-data'>
         <h2>Explore our van options</h2>
+        <div className="filter">
+            <button style={getButtonStyle("simple")}onClick={() => setTypeFilter("simple")}>Simple</button>
+            <button style={getButtonStyle("luxury")} onClick={() => setTypeFilter("luxury")}>Luxury</button>
+            <button style={getButtonStyle("rugged")} onClick={() => setTypeFilter("rugged")}>Rugged</button>
+           {typeFilter? <button className="clear" onClick={() => setTypeFilter(null)}>Clear filters</button>:null}
+        </div>
         <div className='vans'>
         {isLoading ?<p className="loading"><AiOutlineLoading3Quarters className="spin"/>Loading...</p> : renderVans}
         </div>
